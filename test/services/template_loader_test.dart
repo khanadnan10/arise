ddf3ac6261be34eb3c4613048da4dfdb155db941
storm <adnanchom100@gmail.com>
2026-07-30 16:32:58 +0530

    test: add template loader unit tests

--- test/services/template_loader_test.dart
+++ test/services/template_loader_test.dart
@@ -1,25 +1,79 @@
 import 'package:test/test.dart';
 
+import 'package:arise/src/services/template_loader.dart';
+
 void main() {
+  late TemplateLoader loader;
+
+  setUp(() {
+    loader = TemplateLoader();
+  });
+
   group('TemplateLoader', () {
-    test(
-      'loads valid yaml',
-      () async {},
-    );
+    test('loads valid template', () async {
+      final template = await loader.load(
+        'test/fixtures/clean_template.yaml',
+      );
 
-    test(
-      'throws when file does not exist',
-      () async {},
-    );
+      expect(
+        template.name,
+        'Clean Architecture',
+      );
 
-    test(
-      'throws on malformed yaml',
-      () async {},
-    );
+      expect(
+        template.folders.length,
+        2,
+      );
 
-    test(
-      'loads empty package list',
-      () async {},
-    );
+      expect(
+        template.packages.length,
+        2,
+      );
+
+      expect(
+        template.files.length,
+        1,
+      );
+    });
+
+    test('loads empty template', () async {
+      final template = await loader.load(
+        'test/fixtures/empty_template.yaml',
+      );
+
+      expect(template.folders, isEmpty);
+      expect(template.packages, isEmpty);
+      expect(template.files, isEmpty);
+    });
+
+    test('throws if file does not exist', () async {
+      expect(
+        () => loader.load(
+          'does_not_exist.yaml',
+        ),
+        throwsException,
+      );
+    });
+
+    test('throws for malformed yaml', () async {
+      expect(
+        () => loader.load(
+          'test/fixtures/malformed.yaml',
+        ),
+        throwsException,
+      );
+    });
+
+    test('supports missing folders key', () async {});
+
+    test('supports missing packages key', () async {});
+
+    test('supports missing files key', () async {});
+
+    test('supports package dev flag', () async {});
+
+    test('supports package version', () async {});
+
+    test('supports duplicate packages', () async {});
   });
 }
